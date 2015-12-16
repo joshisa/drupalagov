fd766874734040d70f03f83830fcdaa094adea44
Sanjay Joshi <joshisa@us.ibm.com>
2015-12-16 00:59:15 -0500

    Revert "Added settings.php"
    This reverts commit 0e5c71abe38e41fce9a2f70c490a9d5e7276f3a6.

--- fetchConfig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm ./bluezone/configtweaks/settings.php
-cf files drupalagov /app/htdocs/agov-7.x-3.0/sites/default/settings.php > ./bluezone/configtweaks/settings.php
-sed -i -e '1,3d' ./bluezone/configtweaks/settings.php
-
